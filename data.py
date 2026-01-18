665aede0df6fde2f4d2810a179cd7f0161f4e48b
Mohit Kunwar <mk3581@srmist.edu.in>
2026-01-18 23:14:09 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,7 @@
+question_data = [
+    {"text": "A slug's blood is green.", "answer": "True"},
+    {"text": "The capital of Australia is Sydney.", "answer": "False"},
+    {"text": "The Great Wall of China is visible from space.", "answer": "False"},
+    {"text": "Lightning never strikes in the same place twice.", "answer": "False"},
+    {"text": "Humans can distinguish between over a trillion different smells.", "answer":  "True"},
+    {"text": "Goldfish only have a memory of three seconds.", "answer": " False"},]
